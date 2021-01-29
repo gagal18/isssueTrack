4165abaf7b1470657c4f6a28b2244218aeb4d6c8
Bojan <gagaleskibojan@gmail.com>
2021-01-29 20:09:42 +0100

    setUp

---
+++ app.js
@@ -0,0 +1,37 @@
+const description = document.getElementById("description"),
+      visibility = document.getElementById("visibility"),
+      assigned = document.getElementById("assigned"),
+      priority = document.getElementById("priority"),
+      submit = document.getElementById("submit"),
+      issue = document.getElementById("issues")
+var arr = JSON.parse(localStorage.getItem("issue")) || [];
+var obj = {
+    issueId : arr.length,
+    description : description.value,
+    visibility : visibility.value,
+    assigned : assigned.value,
+    priority : priority.value,
+}
+function setIssue(){
+    obj = {
+        issueId : arr.length,
+        description : description.value,
+        visibility : visibility.value,
+        assigned : assigned.value,
+        priority : priority.value,
+    }
+    arr.push(obj)
+    localStorage.setItem("issue", JSON.stringify(arr));
+}
+function showIssue(){
+        if(arr.length){
+            arr.forEach(() => {
+                issue.innerHTML = "";
+                for (var i = 0; i < arr.length; i++)
+                issue.innerHTML +=`<li id="arr[${i}]" class="liE">`+ arr[i].description + `</li>`
+        })}else{
+          todoList.innerHTML = ""
+        }
+}
+submit.addEventListener("click", setIssue)
+showIssue()
